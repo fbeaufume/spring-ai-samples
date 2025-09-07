4c3ee1830fa05cc82bbc0bae39642731b113a1ae
Florian Beaufumé <fbeaufume@gmail.com>
2025-09-07 09:38:20 +0200

    Add an empty Spring Boot application

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring AI Basics
+
+This application shows how to get started with Spring AI and Ollama.
+
+It is implemented in Java with Spring Boot, and takes the form of a simple command line application.
---
+++ src/main/java/com/adeliosys/sample/SpringAiBasicsApplication.java
@@ -0,0 +1,17 @@
+package com.adeliosys.sample;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiBasicsApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiBasicsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+    }
+}
